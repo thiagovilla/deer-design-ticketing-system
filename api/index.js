841610be58a72af65fbc0d6c376e579a9829414a
Thiago Villa <thiago@thiagovilla.com>
2025-04-29 14:19:56 -0300

    fix: deploy Express + React on Vercel

--- api/index.js
+++ api/index.js
@@ -19,19 +19,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-app.use("/", indexRouter);
-
 var apiRouter = express.Router();
+apiRouter.use("/", indexRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/tickets", ticketsRouter);
 
-app.use("/api", apiRouter);
-
-// Serve React SPA
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
+app.use("/", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
