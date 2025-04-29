9d18442acf0bfe10609e463233dd3c6182e35e17
Thiago Villa <thiago@thiagovilla.com>
2025-04-29 11:11:40 -0300

    feat: return error JSON not view

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,6 @@ var ticketsRouter = require("./routes/tickets");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -44,13 +40,10 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
 });
 
 module.exports = app;
